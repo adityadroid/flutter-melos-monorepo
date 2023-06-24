3bec620fec76d3e86888031681e3426d6304ebd5
Aditya Gurjar <17155247+adityadroid@users.noreply.github.com>
2023-06-24 18:01:06 +0530

    Create run configurations for each app+flavor

--- apps/merchant_app/lib/counter/view/counter_page.dart
+++ apps/merchant_app/lib/counter/view/counter_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:merchant_app/counter/counter.dart';
 import 'package:merchant_app/l10n/l10n.dart';
-
 class CounterPage extends StatelessWidget {
   const CounterPage({super.key});
 
